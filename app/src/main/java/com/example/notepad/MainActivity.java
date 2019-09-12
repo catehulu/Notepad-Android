034b9b9cc394a8ad03008d99e4e62c613feb4b64
catehulu <sirajjaya@gmail.com>
2019-09-12 11:56:42 +0700

    tambah del button

--- app/src/main/java/com/example/notepad/MainActivity.java
+++ app/src/main/java/com/example/notepad/MainActivity.java
@@ -3,12 +3,34 @@ package com.example.notepad;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class MainActivity extends AppCompatActivity {
 
+    private EditText editText;
+    private FloatingActionButton delButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        delButton = findViewById(R.id.delButton);
+        editText = findViewById(R.id.edtText);
+
+        delButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                // TODO Auto-generated method stub
+
+                //Clear EditText
+                editText.getText().clear();
+
+            }
+        });
     }
 }
